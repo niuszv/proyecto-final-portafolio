b6859b60e071141666add5915cb99dcd6c06b11c
niuszv <niuszv@gmail.com>
2026-07-27 23:36:21 -0400

    agregue un boton

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const temabtn = document.getElementById("temabtn")
+    const temaguardado = localStorage.getItem("tema")
+     if (temaguardado === "claro") {
+        document.body.classList.add("modo-claro")
+     }
+    
+    if (temabtn) {
+        temabtn.addEventListener("click", () => {
+            document.body.classList.toggle("modo-claro");
+            if (document.body.classList.contains("modo-claro")) {
+                localStorage.setItem("tema", "claro");
+            } else {
+                localStorage.setItem("tema", "oscuro")
+            }
+        }); 
+    }
+    
+    
+    
+    
+    
     const borrarbtn = document.getElementById("borrarbtn");
     
     if (borrarbtn) {
